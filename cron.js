bebded3788d1492d8fd04edb1eecde84073aade3
Jatin Poriya <111553605+InterestingPlus@users.noreply.github.com>
2025-05-04 20:27:18 +0530

    Create cron.js

---
+++ cron.js
@@ -0,0 +1,19 @@
+const cron = require("cron");
+const https = require("https");
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  const hour = new Date().getHours();
+  if (hour >= 7 && hour < 23) {
+    https
+      .get("https://whatsapp-chatbot.onrender.com", (res) => {
+        if (res.statusCode === 200)
+          console.log("GET Request sent Successfully!");
+        else console.log("GET Request failed!", res.statusCode);
+      })
+      .on("error", (e) => console.error("Error while Sending request", e));
+  } else {
+    console.log("Skipping ping during inactive hours");
+  }
+});
+
+module.exports = job;
